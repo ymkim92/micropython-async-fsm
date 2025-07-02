04873e854201c51b3f4b6e5fd749665d3ed21769
Youngmin Kim <ymkim92@gmail.com>
2025-07-02 19:20:07 +1000

    allow multiple values for transition table key

--- src/async_fsm/fsm_async.py
+++ src/async_fsm/fsm_async.py
@@ -1,8 +1,10 @@
 try:
     import uasyncio as asyncio
+    from ucollections import defaultdict
 except ImportError:
     # for testing purposes, use standard asyncio if uasyncio is not available
     import asyncio
+    from collections import defaultdict
 
 _ = asyncio  # Silence Ruff F401: used for compatibility with uasyncio
 
@@ -22,13 +24,15 @@ class AsyncFSM:
     def __init__(self, initial_state, states=None, transition_table=None):
         self.state = initial_state
         self.states = states or {}
-        self.transition_table = transition_table or {}  # key: (state_name, message_id)
+        self.transition_table = transition_table or defaultdict(
+            list
+        )  # key: (state_name, message_id)
 
     def add_state(self, state):
         self.states[state.name] = state
 
     def add_transition(self, from_state, message_id, to_state_name, guard=None, action=None):
-        self.transition_table[(from_state, message_id)] = (to_state_name, guard, action)
+        self.transition_table[(from_state, message_id)].append((to_state_name, guard, action))
 
     async def start(self, ctx):
         await self.state.on_enter_state(ctx)
@@ -37,14 +41,14 @@ class AsyncFSM:
         key = (self.state.name, message.id)
         transition = self.transition_table.get(key)
 
-        if transition:
-            to_state_name, guard, action = transition
+        for to_state_name, guard, action in transition:
             if guard is None or guard(ctx, message):
                 if action:
                     await action(ctx, message)
                 await self.state.on_exit_state(ctx)
                 self.state = self.states[to_state_name]
                 await self.state.on_enter_state(ctx)
+                break
 
     def current_state(self):
         return self.state.name
